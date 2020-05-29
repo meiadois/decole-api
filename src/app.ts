9e88932159691a3254390b96581ca071558f0ae3
GuilhermeCunha <guiscunha@gmail.com>
2020-05-29 15:50:21 -0300

    Ajuste deploy

--- src/app.ts
+++ src/app.ts
@@ -45,7 +45,7 @@ class App {
   private routes (): void {
     this.express.post('/deploy', AutoDeployService.deploy)
     this.express.get('/', function (req, res) {
-      return res.json({ message: 'V5' })
+      return res.json({ message: 'V6' })
     })
 
     this.express.use('/v1', routes)
